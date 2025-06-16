2a48ed2aabe2e143a74c6ac46cccc249d841f300
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2025-06-16 20:34:44 +0100

    Change UserEndpoints to async methods. They now perform asynchronously the same functionality.

--- Endpoints/UserEndpoints.cs
+++ Endpoints/UserEndpoints.cs
@@ -17,18 +17,18 @@ namespace Project.Endpoints
                 return Results.Ok(context.user.ToList());
             });
 
-            app.MapGet("/Users/{UserId}", (UserDbContext context, int UserId) =>
+            app.MapGet("/Users/{UserId}", async (UserDbContext context, int UserId) =>
             {
-                var userList = context.user.ToList();
+                var userList = await context.user.ToListAsync();
                 var specificUser = userList.FirstOrDefault(i => i.UserId == UserId);
                 return specificUser is not null ? Results.Ok(specificUser) : Results.NotFound($"Item with ID {UserId} not found.");
             });
 
-            app.MapPost("/Users", (User user, UserDbContext context) =>
+            app.MapPost("/Users", async (User user, UserDbContext context) =>
             {
                 user.Password = BCrypt.Net.BCrypt.EnhancedHashPassword(user.Password, 13);
-                context.Add(user);
-                context.SaveChanges();
+                await context.AddAsync(user);
+                await context.SaveChangesAsync();
                 return Results.Created();
             });
 
@@ -37,10 +37,9 @@ namespace Project.Endpoints
 
             //});
 
-            app.MapDelete("/Users/{UserId}", (UserDbContext context, int UserId) =>
-            {
-                return context.user.Where(c => c.UserId == UserId).ExecuteDelete() > 0 ? Results.Ok($"User with ID {UserId} was successfully deleted") : Results.NotFound("User ID specific does not exist");
-
+            app.MapDelete("/Users/{UserId}", async (UserDbContext context, int UserId) =>
+            {          
+                return await context.user.Where(c => c.UserId == UserId).ExecuteDeleteAsync() > 0 ? Results.Ok($"User with ID {UserId} was successfully deleted") : Results.NotFound("User ID specific does not exist");
             });
 
         }
