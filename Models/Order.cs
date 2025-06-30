f5307a6ecfee01ade2f0ce411d01fcd5ee2847c6
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2025-06-30 15:56:34 +0100

    Readded Data Annotations to User, Product and Order Models.

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project.Models
 {
@@ -8,9 +9,13 @@ namespace Project.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OrderId { get; set; }
+        [Required(ErrorMessage = "User ID is required")]
         public required int UserId { get; set; }
+        [Required(ErrorMessage = "Total price is required"), Range(0.01, 100000, ErrorMessage = "Total price needs to be a value between 1 cent and 100000€")]
         public required float TotalPrice { get; set; }
+        [Required(ErrorMessage = "Status is required")]
         public required string Status { get; set; }
+        Add commentMore actions
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
