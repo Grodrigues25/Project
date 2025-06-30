812aa1e76b94dbde82dc0482ef705af59e9ebe8e
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2025-06-30 15:04:09 +0100

    Data annotation added to the Data Models to guarantee data consistency and integrity at object level.
    Performed migrations to propagate the changes to the SQL DB.

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project.Models
 {
@@ -9,9 +10,13 @@ namespace Project.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [Required(ErrorMessage = "First name is required")]
         public required string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
         public required string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required"), EmailAddress(ErrorMessage = "Invalid email address format")]
         public required string Email { get; set; }
+        [Required(ErrorMessage = "Password is required"), MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
         public required string Password { get; set; }
         public bool IsAdmin { get; set; } = false;
     }
